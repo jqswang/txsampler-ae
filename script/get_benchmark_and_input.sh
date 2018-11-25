cedf3378d05870f540c126e205c1a9d8680fe687
bugubugu123 <qwang06@email.wm.edu>
2018-11-25 16:59:20 -0500

    Fixed another error

--- script/get_benchmark_and_input.sh
+++ script/get_benchmark_and_input.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Download the benchmark suite ..."
 git clone https://github.com/jqswang/txsampler_benchmark.git /data/txsampler_benchmark
+cd /data/txsampler_benchmark/lib && make
 
 echo "Download the benchmark input and decompress it ... It may take long time..."
 cd /data/txsampler_benchmark \
-  && python /opt/download_gdrive.py 1vf_HbWKNQROHI5XIXIAFfHHfkYJYe1yu tsx_input.tar.bz2 \
+  && python /opt/script/download_gdrive.py 1vf_HbWKNQROHI5XIXIAFfHHfkYJYe1yu tsx_input.tar.bz2 \
   && bzip2 -d tsx_input.tar.bz2 \
   && tar -xvf tsx_input.tar
